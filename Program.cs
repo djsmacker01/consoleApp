f0784edef9579457b91571c55d846e2c7bc2f4be
Success Adedeji Adewale Nurudeen <djsmacker01@gmail.com>
2021-03-14 15:30:00 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace consoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter The First Interger");
+            int first = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter The Second Interger");
+            int second = Convert.ToInt32(Console.ReadLine());
+
+            int result = first + second;
+            Console.WriteLine($"The result is {result}");
+
+            Console.ReadKey();
+        }
+    }
+}
