27f901bb5079513131dac47594f1c71524a5ecdd
Success Adedeji Adewale Nurudeen <djsmacker01@gmail.com>
2021-03-15 22:14:07 +0000

    types conversion

--- Program.cs
+++ Program.cs
@@ -6,16 +6,46 @@ namespace consoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter The First Interger");
-            int first = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter The Second Interger");
-            int second = Convert.ToInt32(Console.ReadLine());
+            //TYPE CONVERSION
 
-            int result = first + second;
-            Console.WriteLine($"The result is {result}");
+            //IMPLICIT CONVERSION
 
-            Console.ReadKey();
+            double b = 12.87;
+            int a = 4;
+            b = b + a;
+
+            Console.WriteLine($" The result of b is now {b}");
+
+
+
+
+
+
+            //Console.WriteLine("Enter The First Interger");
+            //int first = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter The Second Interger");
+            //int second = Convert.ToInt32(Console.ReadLine());
+
+            //int result = first + second;
+            //Console.WriteLine($"The result is {result}");
+
+            //Console.ReadKey();
+
+            // working with substring
+
+            //string testString = "Hello guys, im learnig C#. wow, it was awesome";
+
+            //string partWithoutLength = testString.Substring(10);
+            // string partWithLength = testString.Substring(5, 8);
+            //int charPosition = testString.IndexOf('h');
+
+             //Console.WriteLine(partWithLength);
+            //Console.WriteLine(partWithoutLength);
+            //Console.WriteLine(charPosition);
+
+            //Console.ReadKey();
         }
     }
 }
